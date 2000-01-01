56e1ac1b0f49eeaf576b39ce05e8ecc47f472732
BeagleBone Black <kairotavares@gmail.com>
2000-01-01 21:48:49 +0000

    DNS updater

--- dns_updater.sh
+++ dns_updater.sh
@@ -2,13 +2,16 @@
 
 #Constants
 EXTERNAL_IP_FILE=eip.tmp
-INTERNAL_IP_FILE=iip.tmp
 LOG_FILE=ipchanges.log
 SLEEP_TIME=1m
+DNS_FILE=$1
 
-#Commands
+#running forever
+while [ 1 ]; do
+
+	#Commands
 DATE=$(date)
-EXTERNAL_IP=`curl ipecho.net/plain`
+EXTERNAL_IP=$(curl ipecho.net/plain)
 TEMP_EXTERNAL_IP=$(cat $EXTERNAL_IP_FILE)
 
 IPADDRESS_STRING=$(cat <<EOF
@@ -22,12 +25,6 @@ IPADDRESS_STRING=$(cat <<EOF
 		mx(_a, _a)
 EOF
 )
-
-
-DNS_FILE=$1
-
-#running forever
-while [ 1 ]; do
 	
 	#TODO See if dns file exits 
 
@@ -50,8 +47,8 @@ while [ 1 ]; do
 		echo "[$DATE: EXTERNAL_IP] $TEMP_EXTERNAL_IP to $EXTERNAL_IP" > $LOG_FILE
 		echo $EXTERNAL_IP > $EXTERNAL_IP_FILE
 		echo "IPs not match"
-		echo "$IPADDRESS_STRING" > $1
-		git commit -a -m 'DNS updater'
+		echo $IPADDRESS_STRING > $1
+		git commit -a -m "DNS updater"
 		git push origin
 	fi
 	echo "-----------------------------------------"
