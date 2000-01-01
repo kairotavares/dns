4e350cdf864a807bda8f77c92c4b9f0732455c9b
root <root@dhcppc11.(none)>
2000-01-01 21:17:15 +0000

    DNS updater

--- kairotavares.net.lua
+++ kairotavares.net.lua
@@ -1,12 +1,8 @@
--- File: example.com.lua
--- Variable _a is replaced with zone name by LuaDNS
--- _a = "example.com"
+		-- A Records
+		a(_a, "201.4.220.35")
 
--- A Records
-a(_a, "201.4.220.35")
+		-- CNAME Records
+		cname("www", _a)
 
--- CNAME Records
-cname("www", _a)
-
--- MX Records
-mx(_a, _a)
+		-- MX Records
+		mx(_a, _a)
