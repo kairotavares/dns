ad9adea465b3050a97e545182a0c6a5565f04515
root <root@dhcppc11.(none)>
2014-02-13 09:47:45 +0000

    DNS updater

--- kairotavares.net.lua
+++ kairotavares.net.lua
@@ -1,15 +1,5 @@
 		-- A Records
-		a(_a, "<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>503 Service Unavailable</title>
-</head><body>
-<h1>Service Unavailable</h1>
-<p>The server is temporarily unable to service your
-request due to maintenance downtime or capacity
-problems. Please try again later.</p>
-<hr>
-<address>Apache Server at ipecho.net Port 80</address>
-</body></html>")
+		a(_a, "201.9.53.174")
 
 		-- CNAME Records
 		cname("www", _a)
